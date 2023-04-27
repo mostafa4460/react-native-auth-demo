e8d8e88455f098ef79ef36c4f6801e95090b510e
mostafa4460 <mostafa.nassr88@gmail.com>
2023-04-27 15:44:17 -0400

    add error handling to signup call

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -2,13 +2,21 @@ import { useState } from "react";
 import AuthContent from "../components/Auth/AuthContent";
 import { createUser } from "../util/auth";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { Alert } from "react-native";
 
 function SignupScreen() {
   const [isLoading, setIsloading] = useState(false);
 
   const signupHandler = async ({ email, password }) => {
     setIsloading(true);
-    await createUser(email, password);
+    try {
+      await createUser(email, password);
+    } catch (err) {
+      Alert.alert(
+        "Authentication failed",
+        "Please check your input and try again later."
+      );
+    }
     setIsloading(false);
   };
 
