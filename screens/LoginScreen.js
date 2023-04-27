87dc33c1bbd199e5e04486e02074943952ef6bbd
mostafa4460 <mostafa.nassr88@gmail.com>
2023-04-27 15:43:05 -0400

    add login validation to LoginScreen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,27 @@
-import AuthContent from '../components/Auth/AuthContent';
+import { useState } from "react";
+import AuthContent from "../components/Auth/AuthContent";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { login } from "../util/auth";
+import { Alert } from "react-native";
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  const [isLoading, setIsloading] = useState(false);
+
+  const loginHandler = async ({ email, password }) => {
+    setIsloading(true);
+    try {
+      await login(email, password);
+    } catch (err) {
+      Alert.alert("Authentication failed", "Please check your credentials...");
+    }
+    setIsloading(false);
+  };
+
+  if (isLoading) {
+    return <LoadingOverlay message="Validating credentials..." />;
+  }
+
+  return <AuthContent isLogin onAuthenticate={loginHandler} />;
 }
 
 export default LoginScreen;
