c217794e8f93a444ab448012c25092f4cf937bb8
mostafa4460 <mostafa.nassr88@gmail.com>
2023-05-01 22:02:21 -0400

    fetch protected message in WelcomeScreen

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native';
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function WelcomeScreen() {
+  const [message, setMessage] = useState("");
+  const { token } = useContext(AuthContext);
+
+  useEffect(() => {
+    const fetchMessage = async () => {
+      const response = await axios.get(
+        `https://expense-tracker-c0f91-default-rtdb.firebaseio.com/message.json?auth=${token}`
+      );
+      setMessage(response.data);
+    };
+    fetchMessage();
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{message}</Text>
     </View>
   );
 }
@@ -14,13 +31,13 @@ export default WelcomeScreen;
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 32,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 8,
   },
 });
